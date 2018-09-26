810303efa4bdc8579715422d8e21e585417fed2e
caixian <qq52577517@163.com>
2018-09-26 15:23:07 +0800

    use react-navigation

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Provider} from 'react-redux';
 import {store,persistor} from './redux/store';
-import Root from './route/root';
+import Root from './screen/root';
 import { PersistGate } from 'redux-persist/integration/react';
 
 export default class App extends Component {
